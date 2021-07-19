aa76a0660c98cc344ca5d7a52ce8d07e5f1bf0fb
StormZZ001 <976183601@qq.com>
2021-07-19 19:22:31 +0800

    2.安装element-ui和基础使用

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+
+	<el-radio-group v-model="radio">
+		<el-radio label="线上品牌商赞助"></el-radio>
+		<el-radio label="线下场地免费"></el-radio>
+	</el-radio-group>
+
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  },
+  data() {
+  	return {
+  		radio:""
+  	}
+  },
+  methods: {
+
+  },
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>

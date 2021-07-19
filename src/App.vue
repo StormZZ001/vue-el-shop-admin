62b73cf8d7988a6a3d876a0f8d279fe672dd8a32
StormZZ001 <976183601@qq.com>
2021-07-19 20:25:54 +0800

    4.vue-router二次封装一

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-
-	<el-radio-group v-model="radio">
-		<el-radio label="线上品牌商赞助"></el-radio>
-		<el-radio label="线下场地免费"></el-radio>
-	</el-radio-group>
-
+    <router-view />
   </div>
 </template>
 

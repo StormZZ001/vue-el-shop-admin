92e85200e81aca375555e5772ed8bd5d374b3e7c
StormZZ001 <976183601@qq.com>
2021-07-20 19:51:00 +0800

    10.主布局开发

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,12 +1,39 @@
 <template>
-	<div>
-		主布局
-		<router-view></router-view>
-	</div>
+  <div>
+    <el-container
+      style="position: absolute; left: 0; top: 0; bottom: 0; right: 0; overflow: hidden">
+      <el-header>Header</el-header>
+      <el-container style="height: 100%; padding-bottom: 60px">
+        <!-- 侧边布局 -->
+        <el-aside width="200px">
+          <li v-for="i in 100" :key="i">{{ i }}</li>
+        </el-aside>
+        <!-- 主布局 -->
+        <el-main>
+          <li v-for="i in 100" :key="i">{{ i }}</li>
+        </el-main>
+      </el-container>
+    </el-container>
+
+    <!-- <router-view></router-view> -->
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+}
 </style>

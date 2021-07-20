32d3b885beb441c919ae057ca0354292b49f7d7d
StormZZ001 <976183601@qq.com>
2021-07-20 19:13:49 +0800

    7.登录页UI界面构建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,56 @@
 <template>
 	<div>
-		登录
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-sm-9 col-md-7 col-lg-5 m-auto pt-5">
+					<div class="card mt-5">
+						<div class="card-header bg-white">
+							<h3 class="text-center mb-0 text-secondary">
+								UNI-ADMIN
+							</h3>
+						</div>
+						<div class="card-body">
+							<el-form ref="form" :model="form">
+								<el-form-item>
+									<el-input v-model="form.username"
+									size="medium"
+									placeholder="请输入用户名">
+									</el-input>
+								</el-form-item>
+								<el-form-item>
+									<el-input v-model="form.password"
+									size="medium" type="password"
+									placeholder="请输入密码">
+									</el-input>
+								</el-form-item>
+								<el-form-item>
+									<el-button type="primary" size="medium"
+									class="w-100">
+										立即登录
+									</el-button>
+								</el-form-item>
+							</el-form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
 	</div>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				form:{
+					username:"",
+					password:""
+				}
+			}
+		},
+	}
 </script>
 
 <style>

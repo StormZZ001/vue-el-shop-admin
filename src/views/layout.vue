ac1214e3a7deb1f7967ec9742a7cfdd7dcf7a623
StormZZ001 <976183601@qq.com>
2021-07-21 19:40:59 +0800

    13.全局顶部导航开发二

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,29 +1,34 @@
 <template>
   <div>
     <el-container
-      style="position: absolute; left: 0; top: 0; bottom: 0; right: 0; overflow: hidden"
+      style="
+        position: absolute;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        overflow: hidden;
+      "
     >
       <el-header class="d-flex align-items-center" style="background: #545c64">
         <a class="h5 text-light mb-0 mr-auto">UNI-ADMIN</a>
         <el-menu
-          :default-active="navBarIndex"
+          :default-active="navBar.active"
           mode="horizontal"
           @select="handleSelect"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-menu-item index="1">首页</el-menu-item>
-          <el-menu-item index="2">商品</el-menu-item>
-          <el-menu-item index="3">订单</el-menu-item>
-          <el-menu-item index="4">会员</el-menu-item>
-          <el-menu-item index="5">设置</el-menu-item>
-
+          <el-menu-item :index="index | numToString"
+            v-for="(item,index) in navBar.list"
+            :key="index">{{item.name}}</el-menu-item>
           <el-submenu index="100">
             <template slot="title">
               <el-avatar
                 size="small"
-                src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+                src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+              ></el-avatar>
               summer
             </template>
             <el-menu-item index="100-1">修改</el-menu-item>
@@ -34,22 +39,22 @@
       <el-container style="height: 100%; padding-bottom: 60px">
         <!-- 侧边布局 -->
         <el-aside width="200px">
-			<el-menu default-active="2" @click="slideSelect">
-              <el-submenu index="1">
-                <template slot="title">
-                  <i class="el-icon-location"></i>
-                  <span>导航一</span>
-                </template>
-              </el-submenu>
-              <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-              </el-menu-item>
-              <el-menu-item index="3">
-                <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
-              </el-menu-item>
-			</el-menu>
+          <el-menu default-active="2" @click="slideSelect">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+          </el-menu>
         </el-aside>
         <!-- 主布局 -->
         <el-main>
@@ -63,19 +68,35 @@
 </template>
 
 <script>
+import Common from '../common/mixins/common'
 export default {
+  mixins:[Common],
   data() {
     return {
-      navBarIndex: "1",
+      navBar: {
+        active: "0",
+        list: [
+          { name: "首页" },
+          { name: "商品" },
+          { name: "订单" },
+          { name: "会员" },
+          { name: "设置" },
+        ],
+      },
     };
   },
+  // filters:{
+  //   numToString(value){
+  //     return value.toString()
+  //   }
+  // },
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
     },
-	slideSelect(key, keyPath) {
-	  console.log(key, keyPath);
-	},
+    slideSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
   },
 };
 </script>

ffba9c50645f7e5ac11dd37f80cfd3726c071621
StormZZ001 <976183601@qq.com>
2021-07-23 20:22:31 +0800

    22.安装echarts和使用

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -56,22 +56,22 @@
 			<el-col :span="12">
 				<el-card class="box-card" style="height: 370px;"
 				shadow="never">
-					<div slot="header" class="clearfix">
-						<span>卡片名称</span>
+					<div slot="订单总数统计" class="clearfix">
+						<span>订单数量</span>
 						<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
 					</div>
-					<div class="text item"></div>
+					<!-- 统计图容器 -->
+					<div ref="myChart"
+					style="width: 100%;height: 270px;"></div>
+
 				</el-card>
 			</el-col>
 		</el-row>
-
-
-
-
 	</div>
 </template>
 
 <script>
+let echarts = require('echarts')
 	export default {
 		data() {
 			return {
@@ -111,7 +111,112 @@
 			getCol(total) {
 				return `col-${12/total}`;
 			}
-		}
+		},
+    //dom渲染完毕
+    mounted(){
+      //画统计图
+      this.drawLine()
+    },
+    methods:{
+      drawLine(){
+        //初始化echarts实例
+        let myChart = echarts.init(this.$refs.myChart)
+        //配置参数
+        myChart.setOption({
+          tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            label: {
+                backgroundColor: '#6a7985'
+            }
+        }
+    },
+    legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series: [
+        {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            label: {
+                show: true,
+                position: 'top'
+            },
+            areaStyle: {},
+            emphasis: {
+                focus: 'series'
+            },
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+        })
+      }
+    }
 	}
 </script>
 

c6c3b69f9aaa8f4bcf53746a3fb1499ce99d4d49
StormZZ001 <976183601@qq.com>
2021-07-25 17:07:44 +0800

    27.相册列表组件开发二

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -4,17 +4,17 @@
 		  <el-header class="d-flex align-items-center border-bottom">
 			  <!-- 头部 -->
 			  <div class="d-flex mr-auto">
-				  <el-select class="mr-2" placeholder="请选择活动区域"
-				  size="medium"
+				  <el-select class="mr-2" placeholder="请选择图片排序方式"
+				  size="mini" v-model="searchForm.order"
 				  style="width: 150px;">
 				  	<el-option label="区域一" value="shanghai"></el-option>
 				  	<el-option label="区域二" value="beijing"></el-option>
 				  </el-select>
-				  <el-input class="mr-2" size="medium"
+				  <el-input class="mr-2" size="mini" placeholder="输入相册名称" v-model="searchForm.keyword"
 				  style="width: 150px;"></el-input>
 				  <el-button type="success" size="medium">搜索</el-button>
 			  </div>
-			  <el-button type="success" size="medium">创建相册</el-button>
+			  <el-button type="success" size="medium" @click="openAlbumModel(false)">创建相册</el-button>
 			  <el-button type="warning" size="medium">上传图片</el-button>
 		  </el-header>
 		  <el-container>
@@ -31,8 +31,8 @@
                  {{item.num}}<i class="el-icon-arrow-down el-icon--right"></i>
                </span>
                <el-dropdown-menu slot="dropdown">
-                 <el-dropdown-item>修改</el-dropdown-item>
-                 <el-dropdown-item>删除</el-dropdown-item>
+                 <el-dropdown-item @click.stop.native="openAlbumModel({item,index})">修改</el-dropdown-item>
+                 <el-dropdown-item @click.stop.native="albumDel(index)">删除</el-dropdown-item>
                </el-dropdown-menu>
               </el-dropdown>
            </li>
@@ -48,6 +48,24 @@
 			  <!-- 底部 -->
 		  </el-footer>
 		</el-container>
+    <!-- 创建/修改相册 -->
+    <el-dialog
+      title="修改相册"
+      :visible.sync="albumModel">
+      <el-form :model="albumForm" ref="form" label-width="80px">
+        <el-form-item label="相册名称">
+          <el-input v-model="albumForm.name" size="medium" placeholder="请输入相册名称"></el-input>
+        </el-form-item>
+        <el-form-item label="相册排序">
+          <el-input-number v-model="albumForm.order"  :min="0" size="medium"></el-input-number>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer">
+        <el-button @click=" albumModel= false">取消</el-button>
+        <el-button type="primary" @click="confirmAlbumModel">确定</el-button>
+      </span>
+    </el-dialog>
 
 	</div>
 </template>
@@ -61,6 +79,12 @@ export default {
 					keyword:""
 				},
 				albumIndex:0,
+        albumModel:false,
+        albumEditIndex:-1,
+        albumForm:{
+          name:'',
+          order:0
+        },
 				albums:[]
 			}
   },
@@ -80,6 +104,62 @@ export default {
     //切换相册
     albumChange(index){
       this.albumIndex = index
+    },
+    //打开相册修改/创建框
+    openAlbumModel(obj){
+      //修改
+      if(obj){
+        let {item,index} = obj
+        //初始化表单
+        this.albumForm.name = item.name
+        this.albumForm.order = item.order
+        this.albumEditIndex = index
+        //打开模态框
+        return this.albumModel = true
+      }
+      //创建
+      this.albumForm = {
+        name:"",
+        order:0
+      }
+      this.albumEditIndex = -1
+      this.albumModel = true
+    },
+    //点击确定修改/创建相册
+    confirmAlbumModel(){
+      //判断是否为修改
+      if(this.albumEditIndex > -1){
+        this.albumEdit()
+        return this.albumModel = false
+      }
+      //追加albums
+      this.albums.unshift({
+        name:this.albumForm.name,
+        order:this.albumForm.order,
+        num:0
+      })
+      this.albumModel = false
+    },
+    //修改相册
+    albumEdit(){
+      this.albums[this.albumEditIndex].name = this.albumForm.name
+      this.albums[this.albumEditIndex].order = this.albumForm.order
+    },
+    //删除相册
+    albumDel(index){
+      this.$confirm('是否删除该相册', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(() => {
+        this.albums.splice(index,1)
+        this.$message({
+          message: '删除成功',
+          type: 'success'
+        });
+
+      })
+
     }
   }
 }

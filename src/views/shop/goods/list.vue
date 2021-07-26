9dbf7f7b702219bbe87226e544ca259aadcd50f2
StormZZ001 <976183601@qq.com>
2021-07-26 22:02:19 +0800

    39.Tabs标签页开发

--- src/views/shop/goods/list.vue
+++ src/views/shop/goods/list.vue
@@ -1,10 +1,31 @@
 <template>
-	<div>
-		商品列表
+	<div class="bg-white px-3" style="margin:-20px;margin-top:-1rem">
+		<el-tabs v-model="tabIndex" @tab-click="handleClick">
+      <el-tab-pane v-for="(tab,index) in tabs" :label="tab.name" :key="index">{{tab.name}}</el-tab-pane>
+    </el-tabs>
 	</div>
 </template>
 
 <script>
+export default {
+  data() {
+      return {
+        tabIndex: 0,
+        tabs:[
+          {name:'审核中'},
+          {name:'出售中'},
+          {name:'已下架'},
+          {name:'库存预警'}
+        ]
+      };
+    },
+    methods: {
+      //加载数据
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
+}
 </script>
 
 <style>

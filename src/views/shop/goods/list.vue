9b5d8592f9b5fa0d0b13081cbd7f4c1fb4c0e0d1
StormZZ001 <976183601@qq.com>
2021-07-28 20:22:20 +0800

    42.高级搜索开发三

--- src/views/shop/goods/list.vue
+++ src/views/shop/goods/list.vue
@@ -2,53 +2,38 @@
 	<div class="bg-white px-3" style="margin: -20px;margin-top: -1rem;">
 
 		<el-tabs v-model="tabIndex" @tab-click="handleClick">
-		    <el-tab-pane :label="tab.name" :key="tabI"
-			v-for="(tab,tabI) in tabbars">
-
-
-				<div class="d-flex align-items-center">
-					<!-- 左边 -->
-					<el-button type="success" size="mini">发布商品</el-button>
-					<el-button type="danger" size="mini">批量删除</el-button>
-					<!-- 右边 -->
-					<div class="ml-auto" v-show="!superSearch">
-						<el-input v-model="form.keyword" placeholder="要搜索的商品名称" size="mini" style="width:150px;" class="mr-2"></el-input>
-						<el-button type="info" size="mini">搜索</el-button>
-						<el-button size="mini"
-						@click="superSearch = true">高级搜索</el-button>
-					</div>
-
-				</div>
-				<el-card class="box-card bg-light my-3" v-show="superSearch">
-					<div slot="header" class="clearfix" style="margin: -10px;">
-						<span>高级搜索</span>
-						<el-button style="float: right; padding: 3px 0"
-						type="text" @click="superSearch = false"
-						>收起</el-button>
-					</div>
-					<!-- 表单 -->
-          <el-form inline ref="form" :model="form" label-width="80px">
-            <el-form-item label="商品名称" class="mb-0">
-               <el-input v-model="form.name" placeholder="商品名称" size="mini"></el-input>
-            </el-form-item>
-            <el-form-item label="商品编码" class="mb-0">
-               <el-input v-model="form.code" placeholder="商品编码" size="mini"></el-input>
-            </el-form-item>
-            <el-form-item label="商品类型" class="mb-0">
-               <el-select v-model="form.type" size="mini" placeholder="请输入商品类型">
-                 <el-option label="区域一" value="shanghai"></el-option>
-                 <el-option label="区域二" value="beijing"></el-option>
-               </el-select>
-            </el-form-item>
-            <el-form-item label="商品分类" class="mb-0">
-               <el-input v-model="form.category" placeholder="请输入商品名称" size="mini"></el-input>
-            </el-form-item>
-            <el-form-item class="mb-0">
-               <el-button type="info" size="mini" @click="searchEvent">搜索</el-button>
-               <el-button size="mini" @click="clearSearch">清空搜索条件</el-button>
-            </el-form-item>
-          </el-form>
-				</el-card>
+		    <el-tab-pane :label="tab.name" :key="tabI" v-for="(tab,tabI) in tabbars">
+        <ButtonSearch ref="buttonSearch" placeholder="要搜索的商品名称" @search="searchEvent">
+          <!-- 左边 -->
+          <template #left>
+             <el-button type="success" size="mini">发布商品</el-button>
+					   <el-button type="danger" size="mini">批量删除</el-button>
+          </template>
+          <!--高级搜索表单 -->
+          <template #form>
+            <el-form inline ref="form" :model="form" label-width="80px">
+              <el-form-item label="商品名称" class="mb-0">
+                 <el-input v-model="form.name" placeholder="商品名称" size="mini"></el-input>
+              </el-form-item>
+              <el-form-item label="商品编码" class="mb-0">
+                 <el-input v-model="form.code" placeholder="商品编码" size="mini"></el-input>
+              </el-form-item>
+              <el-form-item label="商品类型" class="mb-0">
+                 <el-select v-model="form.type" size="mini" placeholder="请输入商品类型">
+                   <el-option label="区域一" value="shanghai"></el-option>
+                   <el-option label="区域二" value="beijing"></el-option>
+                 </el-select>
+              </el-form-item>
+              <el-form-item label="商品分类" class="mb-0">
+                 <el-input v-model="form.category" placeholder="请输入商品名称" size="mini"></el-input>
+              </el-form-item>
+              <el-form-item class="mb-0">
+                 <el-button type="info" size="mini" @click="searchEvent">搜索</el-button>
+                 <el-button size="mini" @click="clearSearch">清空搜索条件</el-button>
+              </el-form-item>
+            </el-form>
+          </template>
+        </ButtonSearch>
 			</el-tab-pane>
 		</el-tabs>
 
@@ -56,6 +41,7 @@
 </template>
 
 <script>
+import ButtonSearch from '../../../components/common/button-search.vue'
 	export default {
 		data() {
 			return {
@@ -76,6 +62,9 @@
 				}
 			}
 		},
+    components:{
+      ButtonSearch
+    },
 		methods: {
 			// 加载数据
 			handleClick(tab,event) {
